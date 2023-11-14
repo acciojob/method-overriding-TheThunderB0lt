d6f7dc6df37ed88c1235477b2c63844f9e3c7536
TheThunderB0lt <nikithkumarniki160698@gmail.com>
2023-11-14 11:47:25 +0530

    Code Updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,35 @@
+/*
+Method Overriding
+
+Task 1: create a class A, with a method named meth having returning a string "Invoking method from class A"
+Task 2: create a class B which extends class A
+Task 3: create object of class B and call method meth of class A with it
+Task 4: Now override methode meth of class A in class B, which returns the following string "Method is overridden in Extendend class B"
+Task 5: call this overridden method from obj of class B
+ */
+
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+
+        System.out.println(obj.meth());
+    }
+}
+
+class A {
+    public String meth() {
+        return "invoking method from class A";
+    }
+
+}
+
+class B extends A {
+
+    @Override 
+    public String meth() {
+        return "overridden method from obj class B";
+    }
 }
